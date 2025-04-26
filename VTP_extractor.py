14f313c7f1240ab373cacf984f87075bf624e720
Charlie-R10 <108882229+Charlie-R10@users.noreply.github.com>
2025-04-26 20:40:49 +0100

    Update and rename main.py to VTP_extractor.py

--- VTP_extractor.py
+++ VTP_extractor.py
@@ -4,28 +4,7 @@ import pandas as pd
 
 # Create a reader
 reader = vtk.vtkXMLPolyDataReader()
-reader.SetFileName(r"C:\Users\charl\OneDrive\Desktop\Uni work\4th year project\VTP_extractor\inf_data.vtp")
-
-D = 0.5
-Sa = 0.01
-S0 = 20
-a = 1
-L = np.sqrt(D / Sa)
-a_ex = a + (0.7104*3*D)
-
-phi_values = []
-
-x_vals = np.linspace(0, 1, 101)
-
-for x in x_vals:
-    #phi = ((S0 * L) / (2 * D)) * (np.sinh((a - 2 * x) / (2 * L)) / np.cosh(a / (2 * L))
-    #phi = S0 * L * (1 - np.exp(-2 * a_ex / L)) / (2 * D * (1 + np.exp(-2 * a_ex / L)))  # change to sympy.exp
-
-    exp_term = np.exp(-2 * a_ex / L)
-    numerator = S0 * L * (np.cosh(x / L) - exp_term * np.cosh((2 * a_ex - x) / L))
-    denominator = 2 * D * (1 + exp_term)
-    phi = numerator / denominator
-    phi_values.append(phi)
+reader.SetFileName(r"C:\Users\charl\OneDrive\Desktop\Uni work\4th year project\VTP_extractor\inf_data.vtp") #change this to correct filepath
 
 # Read the file
 reader.Update()
@@ -52,12 +31,7 @@ id_point_final = id_points_array[:, 0]
 
 # Create a DataFrame to store the data
 data = pd.DataFrame({
-    'D': D,
-    'Sa': Sa,
-    'S0': S0,
-    'a': a,
     'u_data': u_data,
-    'u_predicted': np.array(phi_values),
     'ID points': id_point_final
 })
 
